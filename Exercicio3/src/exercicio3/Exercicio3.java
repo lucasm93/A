92c08762eccac89c92d6fa610e590c7e5415c777
lucasm93 <you@example.com>
2017-05-01 23:36:54 -0300

    a

---
+++ Exercicio3/src/exercicio3/Exercicio3.java
@@ -0,0 +1,16 @@
+package exercicio3;
+import java.util.Scanner;
+
+public class Exercicio3 {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        //perguntar num vendedores
+        
+        //perguntar num gerentes
+        
+        Concessionaria cons = new Concessionaria(0, 0);
+    }
+    
+}
